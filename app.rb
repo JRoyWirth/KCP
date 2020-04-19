678bead6e3281f9f24fe656e139d8e0c30811f8c
JRoyWirth <47568323+JRoyWirth@users.noreply.github.com>
2020-04-19 11:41:02 -0500

    Add files via upload

---
+++ app.rb
@@ -0,0 +1,157 @@
+# Set up for the application and database. DO NOT CHANGE. #############################
+require "sinatra"                                                                     #
+require "sinatra/reloader" if development?                                            #
+require "sinatra/cookies" 
+require "sequel"                                                                      #
+require "logger"                                                                      #
+require "twilio-ruby"                                                                 #
+require "bcrypt"                                                                 #
+connection_string = ENV['DATABASE_URL'] || "sqlite://#{Dir.pwd}/development.sqlite3"  #
+DB ||= Sequel.connect(connection_string)                                              #
+DB.loggers << Logger.new($stdout) unless DB.loggers.size > 0                          #
+def view(template); erb template.to_sym; end                                          #
+use Rack::Session::Cookie, key: 'rack.session', path: '/', secret: 'secret'           #
+before { puts; puts "--------------- NEW REQUEST ---------------"; puts }             #
+after { puts; }                                                                       #
+#######################################################################################
+
+user_table = DB.from(:user)
+recipe_table = DB.from(:recipe)
+comment_table = DB.from(:comment)
+
+# established the following code before taking any other route
+before do
+
+    puts "params: #{params}"
+    @user_table = user_table
+    @current_user = @user_table.where(id: session["user_id"]).to_a[0]
+
+    # API credentials (found on your Twilio dashboard)
+    account_sid = ENV['TWILIO_ACCOUNT_SID']
+    auth_token = ENV['YOUR_AUTH_TOKEN']
+    @client = Twilio::REST::Client.new(account_sid, auth_token)
+    @from = '+12057517322' # Twilio number
+    @to = '+16307075213' # Your mobile phone number
+end
+
+get "/" do
+    view "index"
+end
+
+post "/login/create" do
+    puts "params: #{params}"
+
+    @user = @user_table.where(email: params["email"]).to_a[0]
+    pp @user
+
+    if @user
+        if BCrypt::Password.new(@user[:pwd]) == params["pwd"]
+            
+            # know the user is logged in -> rack.session is what will show up in dev tools with an encrypted ID
+            session["user_id"] = @user[:id]
+
+            view "create_login"
+        else
+            view "fail"
+        end
+    else 
+        view "fail"
+    end
+end
+
+get "/signup" do
+    view "signup"
+end
+
+post "/signup/create" do
+    puts "params: #{params}"
+    
+    user_table.insert(
+        namefirst: params["namefirst"],
+        namelast: params["namelast"],
+        email: params["email"],
+        pwd: BCrypt::Password.create(params["pwd"])
+    )
+    view "create_user"
+end
+
+get "/home" do
+    puts "params: #{params}"
+
+    pp recipe_table.all.to_a
+    @recipes = recipe_table.all.to_a
+
+    view "home"
+end
+
+get "/submit" do
+    puts "params: #{params}"
+    view "submit"
+end
+
+post "/recipe/create" do
+    puts "params: #{params}"
+      
+
+    recipe_table.insert(
+        user_id: session["user_id"],
+        year: params["year"],
+        kwest: params["kwest"],
+        title: params["title"],
+        shortdesc: params["shortdesc"],
+        whyrecipe: params["whyrecipe"],
+        location: params["location"],
+        subs: params["subs"],
+        directions: params["directions"],
+        meal: params["meal"],
+        meatless: params["meatless"],
+        dairyfree: params["dairyfree"],
+        glutenfree: params["glutenfree"],  
+    )
+
+    view "create_recipe"
+end
+
+#do I need a new get for every webpage? 
+
+get "/recipe/:id" do
+    puts "params: #{params}"
+
+    pp recipe_table.where(id: params["id"]).to_a[0]
+    @recipe = recipe_table.where(id: params["id"]).to_a[0]
+
+    @comment_table = comment_table
+
+    @comment = comment_table.where(recipe_id: @recipe[:id]).to_a[0]
+    
+
+    view "recipe"
+end
+
+get "/recipe/:id/comment/new" do
+    puts "params: #{params}"
+
+    @recipe = recipe_table.where(id: params["id"]).to_a[0]
+
+    view "new_comment"
+end
+
+post "/recipe/:id/comment/create" do
+    puts "params: #{params}"
+    
+    @recipe = recipe_table.where(id: params["id"]).to_a[0]
+
+    comment_table.insert(
+        user_id: session["user_id"],
+        recipe_id: params["id"],
+        like: params["like"],
+        comment: params["comment"]  
+    )
+    
+    view "create_comment"
+end
+
+get "/logout" do
+    session["user_id"] = nil
+    view "create_logout"
+end
---
+++ createdb.rb
@@ -0,0 +1,134 @@
+# Set up for the application and database. DO NOT CHANGE. #############################
+require "sequel"                                                                      #
+connection_string = ENV['DATABASE_URL'] || "sqlite://#{Dir.pwd}/development.sqlite3"  #
+DB = Sequel.connect(connection_string)                                                #
+#######################################################################################
+
+# Database schema - this should reflect your domain model
+DB.create_table! :user do
+  primary_key :id
+  String :namefirst
+  String :namelast
+  String :email
+  String :pwd
+end
+
+DB.create_table! :recipe do
+  primary_key :id
+  foreign_key :user_id
+  String :program
+  String :year
+  String :section
+  String :kwest
+  String :title
+  String :shortdesc
+  String :whyrecipe, text: true
+  String :location
+  String :ingredients, text: true
+  String :subs, text: true
+  String :directions, text: true
+  #String :photo
+  String :headshot
+  String :meal
+  Boolean :meatless
+  Boolean :dairyfree
+  Boolean :glutenfree
+end
+
+DB.create_table! :comment do
+  primary_key :id
+  foreign_key :user_id
+  foreign_key :recipe_id
+  Boolean :like
+  String :comment, text: true
+end
+
+# Insert initial (seed) data
+recipe_table = DB.from(:recipe)
+
+recipe_table.insert(
+                    user_id: "1",
+                    program: "2Y",
+                    year: "2021",
+                    section: "Buckets",
+                    kwest: "Amazing Race",
+                    title: "BBQ Ribs",
+                    shortdesc: "Sous-vide style midwestern BBQ ribs with homemade sauce",
+                    whyrecipe: "Growing up, I only ate 3 foods at resturants: fried catfish, apple pie and ribs but each is best homemade!",
+                    location: "Illinois, U.S.A.",
+                    ingredients: "1 rack BBQ ribs, brown sugar, spices",
+                    subs: "instead of using extra sauce, try reapplying dry rub",
+                    directions: "1. pat ribs dry...",
+                    headshot: "https://images.app.goo.gl/fYWKfTdH3RG5LGGv7",
+                    meal: "Dinner",
+                    meatless: "0",
+                    dairyfree: "1",
+                    glutenfree: "1"
+                    )
+
+recipe_table.insert(
+                    user_id: "2",
+                    program: "JV",
+                    year: "2021",
+                    section: "Buckets",
+                    kwest: "Amazing Race",
+                    title: "Lasagna",
+                    shortdesc: "Lasagna with meat sauce",
+                    whyrecipe: "Every year on Christmas Day my dad would make stuffed shells or lasagna. This lasagna is what dreams are made of",
+                    location: "Scottsdale, Arizona",
+                    ingredients: "1. box lasagna, 8 oz tomato paste, 1 bunch fresh basil",
+                    subs: "use eggplant slices instead of ground meats",
+                    directions: "1. cut italian sausage length-wise...",
+                    headshot: "https://images.app.goo.gl/fYWKfTdH3RG5LGGv7",
+                    meal: "Dinner",
+                    meatless: "0",
+                    dairyfree: "0",
+                    glutenfree: "0"
+                    )
+
+recipe_table.insert(
+                    user_id: "3",
+                    program: "2Y",
+                    year: "2021",
+                    section: "Poets",
+                    kwest: "Amazing Race",
+                    title: "Vegetarian Pulled BBQ Sandy",
+                    shortdesc: "A vege take on a Texas classic BBQ handheld",
+                    whyrecipe: "Being both a diehard Texan and a dedicated vegetarian, sounds tough but this dish proves otherwise!",
+                    location: "Texas",
+                    ingredients: "1 can jackfruit, 8 oz BBQ sauce (thick and spicy)",
+                    subs: "n/a",
+                    directions: "1. put jackfruit and sauce in a slowcooker...",
+                    headshot: "https://images.app.goo.gl/fYWKfTdH3RG5LGGv7",
+                    meal: "Lunch",
+                    meatless: "1",
+                    dairyfree: "1",
+                    glutenfree: "0"
+                    )
+
+user_table = DB.from(:user)
+
+user_table.insert(
+                    namefirst: "Jason",
+                    namelast: "Wirth",
+                    email: "jason.wirth@kellogg.northwestern.edu",
+                    pwd: "1234"
+                    )
+
+user_table.insert(
+                    namefirst: "Sarah",
+                    namelast: "Wirth",
+                    email: "sarahnwirth@gmail.com",
+                    pwd: "1234"
+                    )
+
+user_table.insert(
+                    namefirst: "Sai",
+                    namelast: "Guntari",
+                    email: "sai.guntari@kellogg.northwestern.edu",
+                    pwd: "1234"
+                    )
+
+
+
+puts "Success!"
